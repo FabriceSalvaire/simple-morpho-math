09962bde4f69346d4366c746984189dc2508cae9
Fabrice Salvaire <fabrice.salvaire@orange.fr>
2017-11-22 00:41:05 +0100

    setup update

---
+++ README.txt
@@ -0,0 +1,36 @@
+.. -*- Mode: rst -*-
+
+.. include:: project-links.txt
+.. include:: abbreviation.txt
+
+==================
+ SimpleMorphoMath
+==================
+
+|Pypi License|
+|Pypi Python Version|
+
+|Pypi Version|
+
+Overview
+========
+
+What is SimpleMorphoMath ?
+--------------------------
+
+SimpleMorphoMath is a simple mathematical morphology library for teaching purposes.
+
+Where is the Documentation ?
+----------------------------
+
+The documentation is available on the |SimpleMorphoMathHomePage|_.
+
+How to install it ?
+-------------------
+
+Look at the `installation <http://fabricesalvaire.github.io/simple-morpho-math/installation.html>`_ section in the documentation.
+
+Credits
+=======
+
+Authors: `Fabrice Salvaire <http://fabrice-salvaire.fr>`_
--- setup.py
+++ setup.py
@@ -1,39 +1,66 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 ####################################################################################################
-# 
-# SimpleMorphoMath - A simple mathematical morphology library.
-# Copyright (C) 2012 Salvaire Fabrice
+#
+# SimpleMorphoMath - A simple mathematical morphology library
+# Copyright (C) 2017 Fabrice Salvaire
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>. 
-# 
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 ####################################################################################################
 
 ####################################################################################################
 
-import os
-from distutils.core import setup
-# from setuptools import setup
+import glob
+import sys
+
+from setuptools import setup, find_packages
+setuptools_available = True
 
 ####################################################################################################
 
-execfile('setup_data.py')
+if sys.version_info < (3,):
+    print('SimpleMorphoMath requires Python 3', file=sys.stderr)
+    sys.exit(1)
+
+exec(compile(open('setup_data.py').read(), 'setup_data.py', 'exec'))
+
+####################################################################################################
+
+setup_dict.update(dict(
+    # include_package_data=True, # Look in MANIFEST.in
+    packages=find_packages(exclude=['unit-test']),
+    # scripts=[],
+    # package_data={},
+
+    platforms='any',
+    zip_safe=False, # due to data files
+
+    classifiers=[
+        'Topic :: Scientific/Engineering',
+        'Intended Audience :: Education',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.0',
+        ],
+
+    install_requires=[
+        'numpy',
+    ],
+))
+
+####################################################################################################
 
 setup(**setup_dict)
-
-####################################################################################################
-#
-# End
-#
-####################################################################################################
